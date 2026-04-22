2ecce15d4d5e5a537d11f12b609f9a77de855616
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-04-21 22:00:22 -0300

    Obtendo a moeda selecionada

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
+// Obtendo os elementos do formulario.
+const form = document.querySelector('form');
 const amount = document.getElementById('amount');
+const currency = document.getElementById('currency');
 
 // Manipulando o input amount para receber somente numeros.
 amount.addEventListener('input', () => {
@@ -7,3 +10,10 @@ amount.addEventListener('input', () => {
     // replace pega a expressao e procura dentro da expressao o padrao. ele substui pela segunda propiedade da funcao
     amount.value = amount.value.replace(hasCharactersRegex, '');
 });
+
+// Capturando o evento de submit(enviar) no formulario
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+    console.log(currency.value);
+};
