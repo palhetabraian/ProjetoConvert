69411579c4a7dc60b9333a1d7bf779ea4be27f5c
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-04-21 21:53:00 -0300

    Validando o input

--- scripts.js
+++ scripts.js
@@ -2,5 +2,8 @@ const amount = document.getElementById('amount');
 
 // Manipulando o input amount para receber somente numeros.
 amount.addEventListener('input', () => {
-    console.log(amount.value);
+    const hasCharactersRegex = /\D+/g;
+
+    // replace pega a expressao e procura dentro da expressao o padrao. ele substui pela segunda propiedade da funcao
+    amount.value = amount.value.replace(hasCharactersRegex, '');
 });
