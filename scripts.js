9b09c1f4b0a8a7ffd451327f4f0e43bb1949dccc
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-04-21 21:47:32 -0300

    Obtendo o valor digitado

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
-console.log('Javascript carregado!');
+const amount = document.getElementById('amount');
+
+// Manipulando o input amount para receber somente numeros.
+amount.addEventListener('input', () => {
+    console.log(amount.value);
+});
