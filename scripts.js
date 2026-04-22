b5b3c6391ed1f5f5a56f00ffa7ebaf631de04f39
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-04-21 22:44:45 -0300

    Identificando a moeda

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+// Cotacao de moedas do dia (hipotetico)
+const USD = 4.87;
+const EUR = 5.32;
+const GBP = 6.08;
+
 // Obtendo os elementos do formulario.
 const form = document.querySelector('form');
 const amount = document.getElementById('amount');
@@ -15,5 +20,20 @@ amount.addEventListener('input', () => {
 form.onsubmit = (event) => {
     event.preventDefault();
 
-    console.log(currency.value);
+    switch (currency.value) {
+        case 'USD':
+            convertCurrency(amount.value, USD, 'US$');
+            break;
+        case 'EUR':
+            convertCurrency(amount.value, EUR, '€');
+            break;
+        case 'GBP':
+            convertCurrency(amount.value, GBP, '£');
+            break;
+    }
 };
+
+// Funcao para converter a moeda.
+function convertCurrency(amount, price, symbol) {
+    console.log(amount, price, symbol);
+}
